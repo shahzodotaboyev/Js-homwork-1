af3a96908b4767513f8cc8a87486a4e8b0327311
shahzod <shoxzodbek15@gmail.com>
2024-09-19 22:25:42 +0500

    s

---
+++ app.js
@@ -0,0 +1,143 @@
+// bagin-1
+// let  a = Math.floor(prompt('To`g`ri to`rtburchakning  a tomoniga qiymat bering'));
+// let  b = Math.floor(prompt('To`g`ri to`rtburchakning  b tomoniga qiymat bering'));
+// let yuzasi = a*b
+// let perimetr = 2 * (a + b);
+// console.log(yuzasi);
+// console.log(perimetr);
+
+
+// begin-2
+// let d = prompt('D ga qiymat bering');
+// const p = 3.14;
+// let l = p * d;
+// let r = d / 2;
+// let s = p * r ** 2;
+// console.log(l);
+// console.log(r);
+// console.log(s);
+
+// Begin-3
+// let a = prompt('a ga qiymat bering');
+// let v =  a**3;
+// let s =  6 * a**2 ;
+// console.log(v);
+// console.log(s);
+
+
+// Begin-4
+// let a = prompt('a ga qiymat bering');
+/// let c = prompt('c ga qiymat bering');
+// let v =  a * b * c;
+// let s8 = 2 * (a * b + b * c + a * c);
+// console.log(v);
+// console.log(s);
+
+// begin-5
+// let a = prompt('a ga qiymat berin');
+// let b = prompt('b ga qiymat berin');
+// let m = (parseFloat(a) + parseFloat(b)) / 2;
+// console.log(m);
+
+// begin-6
+// let a = parseFloat(prompt('a ga qiymat berin'));
+// let b = parseFloat(prompt('b ga qiymat berin'));
+// let c = Math.sqrt(a ** 2 + b ** 2);
+// let p = a + b + c;
+// console.log(p);
+
+// begin-7
+// let r1 = prompt('r1ni kiriting:')
+// let r2 = prompt('r2ni kiriting:')
+// const p = 3.14
+// const s1 = p * r1
+// const s2 = p * r2
+// const S = p * (r1 ** 2 - r2 ** 2)
+// console.log(s1);2
+// console.log(s2);
+// console.log(S);
+
+// Begin-8
+// let l = prompt('L ga qiymattt bering:')
+// const p = 3.14
+// let r = (2 * p) / l;
+// let s = p * r ** 2
+// console.log(r);
+// console.log(s);
+
+// Begin-9
+// let x1 = prompt('x1 ga qiymat bering:')
+// let x2 = prompt('x2 ga qiymat bering:')
+// let y1 = prompt('y1 ga qiymat bering:')
+// let y2 = prompt('y2 ga qiymat bering:')
+// const L = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+// console.log(L);
+
+// begin-10
+// let A = 5;
+// let B = 10;
+// let C = 15;
+// let temp = A;
+// A = B;
+// B = C;
+// C = temp;
+// console.log("A ning yangi qiymati: " + A);
+// console.log("B ning yangi qiymati: " + B);
+// console.log("C ning yangi qiymati: " + C);
+
+// Begin-11
+// let x = prompt('x ga qiymat bering:');
+// let y = 4 * (x - 3) ** 6 - 7 * (x - 3) ** 3 + 2;
+// console.log(y);
+
+// Begin-12
+// let a = prompt('A ga qiymat bering:');
+// let qub = a ** 3
+// let oltinchi = a ** 6
+// let toqqichinchi = a ** 9
+// let onbeshinchi = a ** 15
+// console.log("a  ning qubi: " + qub);
+// console.log("a  ning 6tinchi darajasi: " + oltinchi);
+// console.log("a  ning toqqichinchi darajasi: " + toqqichinchi);
+// console.log("a  ning onbeshinchi darajasi: " + onbeshinchi);
+
+// Begin-13
+// let farengeyt = prompt('gradus belgilang:');
+// const tf = (farengeyt - 32)*5/9
+// console.log(tf);
+
+// Begin 14
+// let a = prompt('a belgilang:');
+// let x = prompt('x belgilang:');
+
+// Integer-1
+// let uch_honalison = prompt('uch honali son kiriting:')
+// let birlik = uch_honalison % 10
+// let onlarHonasi = Math.floor((uch_honalison % 100) / 10)
+// console.log("onliklar honasi: " + onlarHonasi);
+
+// integer-2
+// let uch_honalison = prompt ('uch honali son kiriting');
+// let yuzlik = Math.floor((uch_honalison/100));
+// let onlik = Math.floor((uch_honalison%100)/10);
+// let birlik = uch_honalison % 10
+// let jami = yuzlik + onlik + birlik 
+// console.log(jami);
+
+// Integer-3
+// let uch_honalison = prompt ('uch honali son kiriting');
+// let yuzlik = Math.floor((uch_honalison/100));
+// let onlik = Math.floor((uch_honalison%100)/10);
+// let birlik = uch_honalison % 10
+// let teskariSon = (birlik * 100) + (onlik * 10) + yuzlik;
+// console.log("Teskari tartibdagi son: " + teskariSon);
+
+// Integer-4
+// let uch_honalison = prompt('uch honali son kiriting');
+// let yuzlik = Math.floor((uch_honalison / 100));
+// let onlik = Math.floor((uch_honalison % 100) / 10);
+// let birlik = uch_honalison % 10
+// let javop = (onlik * 100) + (yuzlik * 10) + birlik;
+// console.log(javop);
+
+// Integer-5 
